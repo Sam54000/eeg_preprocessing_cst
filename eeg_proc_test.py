88dac15f350e9814b57d410c8aed648a6af49cdf
lgarbar <daniel.garciabarnett@gmail.com>
2024-07-14 20:12:32 -0400

    Initial commit: edits that includes a method to import LSL recorded data

--- eeg_proc_test.py
+++ eeg_proc_test.py
@@ -1,17 +1,92 @@
-from pipeline import CSTpreprocessing as preprocess
+'''
+ASR is a pretty hefty function, so we'll use the imports below to limit the multithreading
+to something reasonable. It won't impact the speed too much anyway.
+'''
 import os
-EEG_filename = '/data2/Projects/NKI_RS2/MoBI/eeg_preprocessing_cst/data/sub-M10901084/raw/sub-M10901084_ses-MOBI1A_run-001.vhdr'
-events_filename = '/data2/Projects/NKI_RS2/MoBI/eeg_preprocessing_cst/data/sub-M10901084/lsl/sub-M10901084_ses-MOBI1A_task-cst_run-001_lsl_events.csv'
-preprocess = preprocess(EEG_filename, events_filename)
-preprocess.set_annotations_to_raw().set_montage()
-preprocess.run_prep()
-preprocess.run_asr()
-try:
-	extract_file = os.path.join('/data2/Projects/NKI_RS2/MoBI/eeg_preprocessing_cst/data/sub-M10901084/', 'sub-M10901084_eeg_extract.edf')
-	preprocess.save(extract_file)
-except:
-	print('if save fails')
-	extract_file = os.path.join('/data2/Projects/NKI_RS2/MoBI/eeg_preprocessing_cst/data/sub-M10901084/', 'sub-M10901084_eeg_extract_raw.fif')
-	raw = preprocess.raw
-	mne.export(raw, extract_file)
+nthreads = "8"
 
+# Set the number of threads for various libraries
+os.environ["OMP_NUM_THREADS"] = nthreads
+os.environ["OPENBLAS_NUM_THREADS"] = nthreads
+os.environ["MKL_NUM_THREADS"] = nthreads
+os.environ["VECLIB_MAXIMUM_THREADS"] = nthreads
+os.environ["NUMEXPR_NUM_THREADS"] = nthreads
+os.environ["NUMBA_NUM_THREADS"] = nthreads
+os.environ["NUMBA_DEFAULT_NUM_THREADS"] = nthreads
+os.environ["OMP_DYNAMIC"] = "FALSE"
+os.environ["OMP_MAX_ACTIVE_LEVELS"] = "1"
+os.environ["GOTO_NUM_THREADS"] = nthreads
+os.environ["OMP_THREAD_LIMIT"] = nthreads
+os.environ["BLIS_NUM_THREADS"] = nthreads
+os.environ["PTHREAD_POOL_SIZE"] = nthreads
+
+
+'''
+The following are the regular imports needed
+'''
+import mne
+from src.eeg_preprocessing_cst.pipeline import CSTpreprocessing as preprocess
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def create_preproc_object(EEG_filename: str, 
+			  events_filename: str, 
+			  prep: bool = False, 
+			  asr: bool = False) -> 'preprocess':
+	preproc = preprocess(EEG_filename, events_filename)
+	preproc.set_annotations_to_raw().set_montage()
+	print(len([annot for annot in preproc.annotations if annot['description'] == 'Crash']))
+	if prep:
+		preproc.run_prep()
+	if asr:
+		preproc.run_asr()
+	
+	return preproc
+	
+def rename_file(fpath):
+	if os.path.exists(fpath):
+		# Get the directory and filename
+		directory, filename = os.path.split(fpath)
+		# Get the base filename without extension
+		base_filename, ext = os.path.splitext(filename)
+		
+		# Count files with similar names
+		similar_files = [f for f in os.listdir(directory) if base_filename in f]
+		count = len(similar_files)
+		
+		# Rename the file with appropriate count
+		new_filename = f"{base_filename}_{count}{ext}"
+		new_filepath = os.path.join(directory, new_filename)
+		
+		# Rename the file
+		os.rename(fpath, new_filepath)
+		print(f"File '{filename}' already exists. Renamed to '{new_filename}'.")
+	else:
+		print("File does not exist. No action needed.")
+		
+	return fpath
+
+
+# Creating preprocessed EEG file
+EEG_filename = '/Users/danielgarcia-barnett/Desktop/Coding/cpCST_data_analysis/data/sub-M10901084/raw/sub-M10901084_ses-MOBI1A_run-001_crop.fif'
+events_filename = '/Users/danielgarcia-barnett/Desktop/Coding/cpCST_data_analysis/data/sub-M10901084/lsl/sub-M10901084_ses-MOBI1A_task-cst_run-001_lsl_events.csv'
+
+prep_condition = True
+asr_condition = False
+preproc = create_preproc_object(EEG_filename, events_filename, prep = prep_condition, asr = asr_condition)
+
+# Saving preprocessed EEG file
+filepath = os.path.join('/Users/danielgarcia-barnett/Desktop/Coding/cpCST_data_analysis/data/sub-M10901084/time_sync_test/cropped', f'sub-M10901084_eeg_extract_prep-{prep_condition}_asr-{asr_condition}')
+filepath = rename_file(filepath)
+
+try:
+	preproc.save(f'{filepath}_preproc.fif')
+except Exception as e:
+	print(e)
+
+try:
+	raw = preproc.raw
+	raw.save(f'{filepath}_raw.fif', overwrite=True)
+except Exception as e:
+    print(e)
--- src/eeg_preprocessing_cst/pipeline.py
+++ src/eeg_preprocessing_cst/pipeline.py
@@ -44,12 +44,70 @@ class CSTpreprocessing:
     """
 
     def __init__(self,
-                 eeg_filename: str | os.PathLike,
-                 events_filename: str | os.PathLike) -> None:
+             eeg_filename: str | os.PathLike,
+             events_filename: str | os.PathLike,
+             markers_filename: str | os.PathLike = None) -> None:
         self.eeg_filename = eeg_filename
         self.events_filename = events_filename
-        self.raw = mne.io.read_raw(eeg_filename, preload=True)
         self.events = pd.read_csv(events_filename)
+        
+        if eeg_filename.endswith('.pqt'):
+            if markers_filename:
+                self.raw = self.load_lsl_eeg_data(eeg_filename, markers_filename)
+            else:
+                # Handle the case where markers_filename is not provided
+                self.raw = self.load_lsl_eeg_data(eeg_filename)
+        else:
+            self.raw = mne.io.read_raw(eeg_filename, preload=True)
+            
+    def load_lsl_eeg_data(self, eeg_fname, markers_fname):
+        # Load EEG data from Parquet file
+        eeg_data_df = pd.read_parquet(eeg_fname)
+
+        # Ensure all columns except 'timestamps' are numeric
+        eeg_data_df = eeg_data_df.apply(pd.to_numeric, errors='coerce')
+
+        # Extract and convert timestamps
+        eeg_timestamps = [datetime.datetime.fromtimestamp(t, tz=pytz.UTC) for t in eeg_data_df['timestamps']]
+        eeg_data_df['dt_timestamps'] = eeg_timestamps
+
+        # Drop the timestamp columns for EEG data
+        eeg_data = eeg_data_df.drop(columns=['timestamps', 'dt_timestamps']).values.T  # Transpose to match (n_channels, n_times) format
+
+        # Load markers from CSV file
+        markers_df = pd.read_csv(markers_fname)
+
+        # Convert marker timestamps
+        marker_timestamps = [datetime.datetime.fromtimestamp(t, tz=pytz.UTC) for t in markers_df['timestamps']]
+        markers_df['dt_timestamps'] = marker_timestamps
+
+        # Calculate relative onsets in seconds
+        start_time = eeg_timestamps[0]
+        onsets = [(t - start_time).total_seconds() for t in markers_df['dt_timestamps']]
+
+        # Assuming the marker CSV has columns 'timestamps' and 'BrainVision_RDA_Markers'
+        descriptions = markers_df['BrainVision_RDA_Markers'].values
+
+        # Create MNE info object
+        sfreq = 1000  # Specify the sampling frequency of your data
+        ch_names = eeg_data_df.drop(columns=['timestamps', 'dt_timestamps']).columns.tolist()  # Use column names as channel names
+        ch_types = ['eeg'] * len(ch_names)  # Assume all channels are EEG
+
+        info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=ch_types)
+
+        # Create RawArray object
+        self.raw = mne.io.RawArray(eeg_data, info)
+
+        # Set the measurement date using set_meas_date method with correct UTC datetime
+        start_time_utc = start_time.astimezone(datetime.timezone.utc)
+        self.raw.set_meas_date(start_time_utc)
+
+        # Create annotations
+        annotations = mne.Annotations(onset=onsets, duration=[0] * len(onsets), description=descriptions)
+        self.raw.set_annotations(annotations)
+
+        # Now you have an MNE Raw object with annotations
+        return self.raw
     
     def set_annotations_to_raw(self) -> 'CSTpreprocessing':
         """Automatically set the annotations on the raw object.
@@ -133,6 +191,24 @@ class CSTpreprocessing:
         self.raw = asr_obj.transform(self.raw)
         self.raw.set_annotations(self.annotations)
         return self
+    
+    def crop(self, tmin, tmax):
+        """crop the data
+
+        Args:
+            tmin: start time
+            tmax: end time
+        """
+        self.raw.crop(tmin=tmin, tmax=tmax)
+        return self
+    
+    def copy(self): # Fix this later so it properly creates a copy of the object rather than just returning a raw object
+        """return a copy of the raw object
+
+        Returns:
+            copy of mne.io.Raw object
+        """
+        return self.raw.copy()
 
     def save(self, filename:str | os.PathLike) -> 'CSTpreprocessing':
         """save the data
